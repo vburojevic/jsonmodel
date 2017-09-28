11d9ac480667692c5f31d1da336162117726c2c2
Vedran Burojević <vedran.burojevic@gmail.com>
2017-09-28 08:41:56 +0200

    Update JSONModel.m

--- JSONModel/JSONModel/JSONModel.m
+++ JSONModel/JSONModel/JSONModel.m
@@ -47,7 +47,7 @@ static JSONKeyMapper* globalKeyMapper = nil;
 
         @autoreleasepool {
             allowedJSONTypes = @[
-                [NSString class], [NSNumber class], [NSDecimalNumber class], [NSArray class], [NSDictionary class], [NSNull class], //immutable JSON classes
+                [NSString class], [NSNumber class], [NSArray class], [NSDictionary class], [NSNull class], //immutable JSON classes
                 [NSMutableString class], [NSMutableArray class], [NSMutableDictionary class] //mutable JSON classes
             ];
 
